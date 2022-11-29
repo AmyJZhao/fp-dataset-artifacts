41c7e1beb370ed842d759c82992fb6cc50e6a920
Amy Zhao <azhao@adobe.com>
2022-11-29 05:40:50 -0600

    Modify source code to be able to evaluate model on SQUAD 2.0 dataset

--- helpers.py
+++ helpers.py
@@ -159,10 +159,33 @@ def prepare_validation_dataset_qa(examples, tokenizer):
 # This function uses start and end position scores predicted by a question answering model to
 # select and extract the predicted answer span from the context.
 # Adapted from https://github.com/huggingface/transformers/blob/master/examples/pytorch/question-answering/utils_qa.py
-def postprocess_qa_predictions(examples,
-                               features,
-                               predictions: Tuple[np.ndarray, np.ndarray],
-                               n_best_size: int = 20):
+def postprocess_qa_predictions(
+    examples,
+    features,
+    predictions: Tuple[np.ndarray, np.ndarray],
+    version_2_with_negative: bool = False,
+    n_best_size: int = 20,
+    null_score_diff_threshold: float = 0.0):
+    """
+    Post-processes the predictions of a question-answering model to convert them to answers that are substrings of the
+    original contexts. This is the base postprocessing functions for models that only return start and end logits.
+    Args:
+        examples: The non-preprocessed dataset (see the main script for more information).
+        features: The processed dataset (see the main script for more information).
+        predictions (:obj:`Tuple[np.ndarray, np.ndarray]`):
+            The predictions of the model: two arrays containing the start logits and the end logits respectively. Its
+            first dimension must match the number of elements of :obj:`features`.
+        version_2_with_negative (:obj:`bool`, `optional`, defaults to :obj:`False`):
+            Whether or not the underlying dataset contains examples with no answers.
+        n_best_size (:obj:`int`, `optional`, defaults to 20):
+            The total number of n-best predictions to generate when looking for an answer.
+        null_score_diff_threshold (:obj:`float`, `optional`, defaults to 0):
+            The threshold used to select the null answer: if the best answer has a score that is less than the score of
+            the null answer minus this threshold, the null answer is selected for this example (note that the score of
+            the null answer for an example giving several features is the minimum of the scores for the null answer on
+            each feature: all features must be aligned on the fact they `want` to predict a null answer).
+            Only useful when :obj:`version_2_with_negative` is :obj:`True`.
+    """
     if len(predictions) != 2:
         raise ValueError(
             "`predictions` should be a tuple with two elements (start_logits, end_logits).")
@@ -176,17 +199,20 @@ def postprocess_qa_predictions(examples,
     example_id_to_index = {k: i for i, k in enumerate(examples["id"])}
     features_per_example = collections.defaultdict(list)
     for i, feature in enumerate(features):
-        features_per_example[
-            example_id_to_index[feature["example_id"]]].append(i)
+        features_per_example[example_id_to_index[feature["example_id"]]].append(
+            i)
 
     # The dictionaries we have to fill.
     all_predictions = collections.OrderedDict()
+    if version_2_with_negative:
+        scores_diff_json = collections.OrderedDict()
 
     # Let's loop over all the examples!
     for example_index, example in enumerate(tqdm(examples)):
         # Those are the indices of the features associated to the current example.
         feature_indices = features_per_example[example_index]
 
+        min_null_prediction = None
         prelim_predictions = []
 
         # Looping through all the features associated to the current example.
@@ -194,45 +220,74 @@ def postprocess_qa_predictions(examples,
             # We grab the predictions of the model for this feature.
             start_logits = all_start_logits[feature_index]
             end_logits = all_end_logits[feature_index]
-            # This is what will allow us to map some the positions in our logits
-            # to span of texts in the original context.
+            # This is what will allow us to map some the positions in our logits to span of texts in the original
+            # context.
             offset_mapping = features[feature_index]["offset_mapping"]
+            # Optional `token_is_max_context`, if provided we will remove answers that do not have the maximum context
+            # available in the current feature.
+            token_is_max_context = features[feature_index].get(
+                "token_is_max_context", None)
+
+            # Update minimum null prediction.
+            feature_null_score = start_logits[0] + end_logits[0]
+            if min_null_prediction is None or min_null_prediction["score"] > feature_null_score:
+                min_null_prediction = {
+                    "offsets": (0, 0),
+                    "score": feature_null_score,
+                    "start_logit": start_logits[0],
+                    "end_logit": end_logits[0],
+                }
 
             # Go through all possibilities for the `n_best_size` greater start and end logits.
-            start_indexes = np.argsort(start_logits)[
-                            -1: -n_best_size - 1: -1].tolist()
-            end_indexes = np.argsort(end_logits)[
-                          -1: -n_best_size - 1: -1].tolist()
+            start_indexes = np.argsort(
+                start_logits)[-1: -n_best_size - 1: -1].tolist()
+            end_indexes = np.argsort(
+                end_logits)[-1: -n_best_size - 1: -1].tolist()
             for start_index in start_indexes:
                 for end_index in end_indexes:
                     # Don't consider out-of-scope answers, either because the indices are out of bounds or correspond
                     # to part of the input_ids that are not in the context.
                     if (
-                            start_index >= len(offset_mapping)
-                            or end_index >= len(offset_mapping)
-                            or offset_mapping[start_index] is None
-                            or offset_mapping[end_index] is None
+                        start_index >= len(offset_mapping)
+                        or end_index >= len(offset_mapping)
+                        or offset_mapping[start_index] is None
+                        or len(offset_mapping[start_index]) < 2
+                        or offset_mapping[end_index] is None
+                        or len(offset_mapping[end_index]) < 2
                     ):
                         continue
                     # Don't consider answers with a length that is either < 0 or > max_answer_length.
-                    if end_index < start_index or \
-                            end_index - start_index + 1 > QA_MAX_ANSWER_LENGTH:
+                    if end_index < start_index or end_index - start_index + 1 > QA_MAX_ANSWER_LENGTH:
+                        continue
+                    # Don't consider answer that don't have the maximum context available (if such information is
+                    # provided).
+                    if token_is_max_context is not None and not token_is_max_context.get(str(start_index), False):
                         continue
 
                     prelim_predictions.append(
                         {
-                            "offsets": (offset_mapping[start_index][0],
-                                        offset_mapping[end_index][1]),
-                            "score": start_logits[start_index] +
-                                     end_logits[end_index],
+                            "offsets": (offset_mapping[start_index][0], offset_mapping[end_index][1]),
+                            "score": start_logits[start_index] + end_logits[end_index],
                             "start_logit": start_logits[start_index],
                             "end_logit": end_logits[end_index],
                         }
                     )
+        if version_2_with_negative and min_null_prediction is not None:
+            # Add the minimum null prediction
+            prelim_predictions.append(min_null_prediction)
+            null_score = min_null_prediction["score"]
 
         # Only keep the best `n_best_size` predictions.
-        predictions = sorted(prelim_predictions, key=lambda x: x["score"],
-                             reverse=True)[:n_best_size]
+        predictions = sorted(prelim_predictions, key=lambda x: x["score"], reverse=True)[
+            :n_best_size]
+
+        # Add back the minimum null prediction if it was removed because of its low score.
+        if (
+            version_2_with_negative
+            and min_null_prediction is not None
+            and not any(p["offsets"] == (0, 0) for p in predictions)
+        ):
+            predictions.append(min_null_prediction)
 
         # Use the offsets to gather the answer text in the original context.
         context = example["context"]
@@ -240,15 +295,43 @@ def postprocess_qa_predictions(examples,
             offsets = pred.pop("offsets")
             pred["text"] = context[offsets[0]: offsets[1]]
 
-        # In the very rare edge case we have not a single non-null prediction,
-        # we create a fake prediction to avoid failure.
-        if len(predictions) == 0 or (
-                len(predictions) == 1 and predictions[0]["text"] == ""):
-            predictions.insert(0, {"text": "empty", "start_logit": 0.0,
-                                   "end_logit": 0.0, "score": 0.0})
+        # In the very rare edge case we have not a single non-null prediction, we create a fake prediction to avoid
+        # failure.
+        if len(predictions) == 0 or (len(predictions) == 1 and predictions[0]["text"] == ""):
+            predictions.insert(
+                0, {"text": "empty", "start_logit": 0.0, "end_logit": 0.0, "score": 0.0})
 
-        all_predictions[example["id"]] = predictions[0]["text"]
-    return all_predictions
+        # Compute the softmax of all scores (we do it with numpy to stay independent from torch/tf in this file, using
+        # the LogSumExp trick).
+        scores = np.array([pred.pop("score") for pred in predictions])
+        exp_scores = np.exp(scores - np.max(scores))
+        probs = exp_scores / exp_scores.sum()
+
+        # Include the probabilities in our predictions.
+        for prob, pred in zip(probs, predictions):
+            pred["probability"] = prob
+
+        # Pick the best prediction. If the null answer is not possible, this is easy.
+        if not version_2_with_negative:
+            all_predictions[example["id"]] = predictions[0]["text"]
+        else:
+            # Otherwise we first need to find the best non-empty prediction.
+            i = 0
+            while predictions[i]["text"] == "":
+                i += 1
+            best_non_null_pred = predictions[i]
+
+            # Then we compare to the null prediction using the threshold.
+            score_diff = null_score - \
+                best_non_null_pred["start_logit"] - \
+                best_non_null_pred["end_logit"]
+            # To be JSON-serializable.
+            scores_diff_json[example["id"]] = float(score_diff)
+            if score_diff > null_score_diff_threshold:
+                all_predictions[example["id"]] = ""
+            else:
+                all_predictions[example["id"]] = best_non_null_pred["text"]
+    return all_predictions, scores_diff_json
 
 
 # Adapted from https://github.com/huggingface/transformers/blob/master/examples/pytorch/question-answering/trainer_qa.py
@@ -286,11 +369,14 @@ class QuestionAnsweringTrainer(Trainer):
         if self.compute_metrics is not None:
             # post process the raw predictions to get the final prediction
             # (from start_logits, end_logits to an answer string)
-            eval_preds = postprocess_qa_predictions(eval_examples,
+            eval_preds, null_odds = postprocess_qa_predictions(eval_examples,
                                                     eval_dataset,
-                                                    output.predictions)
+                                                    output.predictions,
+                                                    True)
             formatted_predictions = [{"id": k, "prediction_text": v}
                                      for k, v in eval_preds.items()]
+            for pred in formatted_predictions:
+                pred["no_answer_probability"] = null_odds[pred["id"]]
             references = [{"id": ex["id"], "answers": ex['answers']}
                           for ex in eval_examples]
 
